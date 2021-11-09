936f97582eb2d3c1a90062d217a1de3ce0986205
samahhamed227 <samahhamed227@gmail.com>
2021-11-09 13:58:35 +0200

    sll

--- src/models/index.js
+++ src/models/index.js
@@ -5,9 +5,13 @@ const { Sequelize, DataTypes } = require('sequelize');
 const userSchema = require('./users.model');
 
 const DATABASE_URL = `${process.env.DATABASE_URL}`;
+const DATABASE_CONFIG = process.env.NODE_ENV === 'production' ?  {
+  dialectOptions: {
+    ssl: {require: true, rejectUnauthorized: false
+  
+  }}}: {}
+const sequelize = new Sequelize(DATABASE_URL, DATABASE_CONFIG);
 
-
-const sequelize = new Sequelize(DATABASE_URL);
 
 module.exports = {
   db: sequelize,
